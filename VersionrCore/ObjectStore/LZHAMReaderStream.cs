a7cee57f783ca2711c2b2301ca81edbae2c5a407
EDruce <EDruce@versionr>
2015-08-06 17:55:49 +1000

    less crashy lzham

--- VersionrCore/ObjectStore/LZHAMReaderStream.cs
+++ VersionrCore/ObjectStore/LZHAMReaderStream.cs
@@ -16,25 +16,32 @@ namespace Versionr.ObjectStore
         private static extern bool DestroyDecompressionStream(IntPtr stream);
 
         [System.Runtime.InteropServices.DllImport("lzhamwrapper", EntryPoint = "DecompressData", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        private static extern int DecompressData(IntPtr stream, byte[] output, int outLength, out bool finished);
+        private static extern int DecompressData(IntPtr stream, IntPtr output, int outLength, out bool finished);
 
         [System.Runtime.InteropServices.DllImport("lzhamwrapper", EntryPoint = "DecompressSetSource", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        private static extern int DecompressSetSource(IntPtr stream, byte[] output, int outLength);
+        private static extern int DecompressSetSource(IntPtr stream, IntPtr output, int outLength);
 
         byte[] m_DecompressionBuffer = null;
         protected override void RefillBuffer(byte[] data, byte[] output, int decompressedSize, bool end)
         {
-            m_DecompressionBuffer = data;
-            DecompressSetSource(m_Decompressor, data, data.Length);
-            bool finished;
-            while (DecompressData(m_Decompressor, output, decompressedSize, out finished) != decompressedSize)
-            {
-                DestroyDecompressionStream(m_Decompressor);
-                m_Decompressor = CreateDecompressionStream(23);
-                DecompressSetSource(m_Decompressor, data, data.Length);
-            }
-            while (!finished)
-                DecompressData(m_Decompressor, output, 0, out finished);
+			unsafe
+			{
+				fixed (byte* input = data)
+				fixed (byte* outptr = output)
+				{
+					m_DecompressionBuffer = data;
+					DecompressSetSource(m_Decompressor, (IntPtr)input, data.Length);
+					bool finished;
+					while (DecompressData(m_Decompressor, (IntPtr)outptr, decompressedSize, out finished) != decompressedSize)
+					{
+						DestroyDecompressionStream(m_Decompressor);
+						m_Decompressor = CreateDecompressionStream(23);
+						DecompressSetSource(m_Decompressor, (IntPtr)input, data.Length);
+					}
+					while (!finished)
+						DecompressData(m_Decompressor, (IntPtr)outptr, 0, out finished);
+				}
+			}
         }
 
         public LZHAMReaderStream(long size, System.IO.Stream baseStream)
