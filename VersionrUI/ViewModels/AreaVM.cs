38935e7e817149d9b7678e1106df6d1cc9b739a5
allindsay <allindsay@versionr>
2015-11-24 14:31:28 +1100

    Started filling in view models

--- VersionrUI/ViewModels/AreaVM.cs
+++ VersionrUI/ViewModels/AreaVM.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using Versionr;
 
 namespace VersionrUI.ViewModels
@@ -6,16 +7,41 @@ namespace VersionrUI.ViewModels
     public class AreaVM
     {
         private Area _area;
+        private string _name;
+        private ObservableCollection<BranchVM> _branches;
 
-        public AreaVM(Area area)
+        public AreaVM(Area area, string name)
         {
             _area = area;
+            _name = name;
+            _branches = new ObservableCollection<BranchVM>();
+
+            RefreshBranches();
+        }
+
+        private void RefreshBranches()
+        {
+            _branches.Clear();
+
+            // TODO consider retaining BranchVM instances
+            foreach (Versionr.Objects.Branch branch in _area.Branches)
+            {
+                _branches.Add(new BranchVM(_area, branch));
+            }
         }
 
         public string Name
         {
             // TODO:
-            get { return String.Empty; }
+            get { return _name; }
+        }
+
+        public ObservableCollection<BranchVM> Branches
+        {
+            get
+            {
+                return _branches;
+            }
         }
     }
 }
--- VersionrUI/ViewModels/BranchVM.cs
+++ VersionrUI/ViewModels/BranchVM.cs
@@ -1,6 +1,27 @@
-﻿namespace VersionrUI.ViewModels
+﻿using System;
+using Versionr;
+
+namespace VersionrUI.ViewModels
 {
     public class BranchVM
     {
+        private Area _area;
+        private Versionr.Objects.Branch _branch;
+
+        public BranchVM(Area area, Versionr.Objects.Branch branch)
+        {
+            _area = area;
+            _branch = branch;
+        }
+
+        public string Name
+        {
+            get { return _branch.Name; }
+        }
+
+        public bool IsCurrent
+        {
+            get { return _area.CurrentBranch == _branch; }
+        }
     }
 }
