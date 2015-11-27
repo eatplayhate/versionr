68be9850f8ca02d46563339aa2102fafbd8e5d2c
acrawford <acrawford@versionr>
2015-11-27 11:04:01 +1100

    Added command to diff alteration records with the current working copy

--- VersionrUI/ViewModels/AlterationVM.cs
+++ VersionrUI/ViewModels/AlterationVM.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using Versionr;
 using Versionr.Objects;
@@ -9,7 +10,8 @@ namespace VersionrUI.ViewModels
 {
     public class AlterationVM
     {
-        public DelegateCommand DiffCommand { get; private set; }
+        public DelegateCommand DiffWithPreviousCommand { get; private set; }
+        public DelegateCommand DiffWithCurrentCommand { get; private set; }
 
         private Alteration _alteration;
         private Area _area;
@@ -25,48 +27,87 @@ namespace VersionrUI.ViewModels
             if (_alteration.NewRecord.HasValue)
                 _newRecord = _area.GetRecord(_alteration.NewRecord.Value);
 
-            DiffCommand = new DelegateCommand(() => Diff(),
-                                              () => _alteration.PriorRecord.HasValue && _alteration.NewRecord.HasValue);
+            DiffWithPreviousCommand = new DelegateCommand(DiffWithPrevious, CanDiffWithPrevious);
+            DiffWithCurrentCommand = new DelegateCommand(DiffWithCurrent, CanDiffWithCurrent);
         }
 
         public AlterationType AlterationType { get { return _alteration.Type; } }
 
         public string Name { get { return _newRecord?.CanonicalName ?? _priorRecord.CanonicalName; } }
 
-        public void Diff()
+        public bool CanDiffWithPrevious()
         {
-            if (_newRecord != null && _priorRecord != null &&
-                !_newRecord.IsDirectory && !_priorRecord.IsDirectory &&
-                _alteration.Type == AlterationType.Update)
+            return _newRecord != null &&
+                   _priorRecord != null &&
+                   !_newRecord.IsDirectory &&
+                   !_priorRecord.IsDirectory &&
+                   _alteration.Type == AlterationType.Update;
+        }
+
+        public bool CanDiffWithCurrent()
+        {
+            return _newRecord != null &&
+                   !_newRecord.IsDirectory &&
+                   _alteration.Type != AlterationType.Delete &&
+                   File.Exists(Path.Combine(_area.Root.FullName, _newRecord.CanonicalName));
+        }
+
+        public void DiffWithPrevious()
+        {
+            if ((_priorRecord.Attributes & Attributes.Binary) == Attributes.Binary ||
+                (_newRecord.Attributes & Attributes.Binary) == Attributes.Binary)
             {
-                if ((_priorRecord.Attributes & Attributes.Binary) == Attributes.Binary ||
-                    (_newRecord.Attributes & Attributes.Binary) == Attributes.Binary)
+                MessageBox.Show(string.Format("Not showing binary differences."));
+            }
+            else
+            {
+                // Displaying modifications
+                string tmpPrior = DiffTool.GetTempFilename();
+                string tmpNew = DiffTool.GetTempFilename();
+                _area.GetMissingRecords(new Record[] { _priorRecord, _newRecord });
+                _area.RestoreRecord(_priorRecord, DateTime.UtcNow, tmpPrior);
+                _area.RestoreRecord(_newRecord, DateTime.UtcNow, tmpNew);
+                LimitedTaskDispatcher.Factory.StartNew(() =>
                 {
-                    MessageBox.Show(string.Format("Not showing binary differences."));
-                }
-                else
-                {
-                    // Displaying local modifications
-                    string tmpPrior = DiffTool.GetTempFilename();
-                    string tmpNew = DiffTool.GetTempFilename();
-                    _area.GetMissingRecords(new Record[] { _priorRecord, _newRecord });
-                    _area.RestoreRecord(_priorRecord, DateTime.UtcNow, tmpPrior);
-                    _area.RestoreRecord(_newRecord, DateTime.UtcNow, tmpNew);
-                    LimitedTaskDispatcher.Factory.StartNew(() =>
+                    try
                     {
-                        try
-                        {
-                            DiffTool.Diff(tmpPrior, _priorRecord.Name, 
-                                          tmpNew, _newRecord.Name,
-                                          _area.Directives.ExternalDiff, false);
-                        }
-                        finally
-                        {
-                            System.IO.File.Delete(tmpPrior);
-                            System.IO.File.Delete(tmpNew);
-                        }
-                    });
-                }
+                        DiffTool.Diff(tmpPrior, String.Format("{0} ({1})", _priorRecord.Name, _alteration.PriorRecord),
+                                      tmpNew, String.Format("{0} ({1})", _newRecord.Name, _alteration.NewRecord),
+                                      _area.Directives.ExternalDiff, false);
+                    }
+                    finally
+                    {
+                        File.Delete(tmpPrior);
+                        File.Delete(tmpNew);
+                    }
+                });
+            }
+        }
+
+        public void DiffWithCurrent()
+        {
+            if ((_newRecord.Attributes & Attributes.Binary) == Attributes.Binary)
+            {
+                MessageBox.Show(string.Format("File: {0} is binary different.", _newRecord.CanonicalName));
+            }
+            else
+            {
+                string tmpNew = DiffTool.GetTempFilename();
+                _area.GetMissingRecords(new Record[] { _newRecord });
+                _area.RestoreRecord(_newRecord, DateTime.UtcNow, tmpNew);
+                LimitedTaskDispatcher.Factory.StartNew(() =>
+                {
+                    try
+                    {
+                        DiffTool.Diff(tmpNew, String.Format("{0} ({1})", _newRecord.Name, _alteration.NewRecord),
+                                      Path.Combine(_area.Root.FullName, _newRecord.CanonicalName), String.Format("{0} (working copy)", _newRecord.Name),
+                                      _area.Directives.ExternalDiff, false);
+                    }
+                    finally
+                    {
+                        File.Delete(tmpNew);
+                    }
+                });
             }
         }
     }
