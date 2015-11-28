f06b42294fbea9696eab322be83830544bc0fb5b
Lewis <Lewis@versionr>
2015-11-28 13:50:24 +1100

    More tweaking of trivial things

--- VersionrCore/Area.cs
+++ VersionrCore/Area.cs
@@ -3600,7 +3600,7 @@ namespace Versionr
 
                 if (spinner == null)
                 {
-                    spinner = Printer.CreateSpinnerPrinter("Deleting...", (obj) => { return string.Format("Deleting {0} objects.", (int)obj); });
+                    spinner = Printer.CreateSpinnerPrinter("Deleting ", (obj) => { return string.Format("{0} objects.", (int)obj); });
                 }
                 spinner.Update(deletionCount++);
 
