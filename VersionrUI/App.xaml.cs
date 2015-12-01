0b712d670850ed190de06fa30c1e6e7241b10286
acrawford <acrawford@versionr>
2015-12-01 11:59:40 +1100

    Displaying timestamps in local format

--- VersionrUI/App.xaml.cs
+++ VersionrUI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
 using Versionr.Utilities;
 
 namespace VersionrUI
@@ -10,6 +12,12 @@ namespace VersionrUI
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            // From http://www.codeproject.com/Articles/31837/Creating-an-Internationalized-Wizard-in-WPF
+            // Ensure the current culture passed into bindings 
+            // is the OS culture. By default, WPF uses en-US 
+            // as the culture, regardless of the system settings.
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
             base.OnStartup(e);
 
             MultiArchPInvoke.BindDLLs();
