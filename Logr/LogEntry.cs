bdb14d6f24b466c20ec191f539b1f0b6500a1c67
ychernikov <ychernikov@versionr>
2015-12-03 11:36:14 +1100

    Committed merge with Logr source code

---
+++ Logr/LogEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Logr
+{
+    public class LogEntry
+    {
+        [XmlAttribute("branch")]
+        public string Branch { get; set; }
+        [XmlAttribute("revision")]
+        public uint Revision { get; set; }
+
+        [XmlElement("author")]
+        public string Author { get; set; }
+        [XmlElement("date")]
+        public string Date { get; set; }
+        [XmlElement("msg")]
+        public string Message { get; set; }
+
+        public LogEntry() {}
+
+        public LogEntry(string branch, uint revision, string author, string data, string message)
+        {
+            Branch = branch;
+            Revision = revision;
+            Author = author;
+            Date = data;
+            Message = message;
+        }
+
+
+    }
+}
