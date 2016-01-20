f4e0edb28180dbbe11882b938f9a6af071e54e3c
jrunting <jrunting@versionr>
2016-01-20 13:20:19 +1100

    updated status to partition files (a la git)

--- Versionr/Commands/FileBaseCommand.cs
+++ Versionr/Commands/FileBaseCommand.cs
@@ -23,6 +23,9 @@ namespace Versionr.Commands
         public bool Tracked { get; set; }
         [Option('i', "ignored", HelpText = "Show ignored files")]
         public bool Ignored { get; set; }
+		[Option('w', "windows", HelpText = "Use backslashes in path display")]
+		public bool WindowsPaths { get; set; }
+
         public override string Usage
         {
             get
