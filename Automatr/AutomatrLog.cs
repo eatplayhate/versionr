4ebc2374eaaf7280c3cd646bb15e4f880c9c6654
ychernikov <ychernikov@versionr>
2016-01-20 17:49:47 +1100

    Added missing files to Automatr.

---
+++ Automatr/AutomatrLog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatr
+{
+    public class AutomatrLog
+    {
+
+        public enum LogLevel
+        {
+            Error = 0,
+            Info = 1,
+            Verbose = 2
+        }
+
+        public static LogLevel Level
+        {
+            get
+            {
+                return Program.Options.Verbose ? LogLevel.Verbose : LogLevel.Info;
+            }
+        }
+
+        public static void Log(string message, LogLevel level = LogLevel.Verbose)
+        {
+            Log(message, true, level);
+        }
+
+        public static void Log(string message, bool newLine, LogLevel level = LogLevel.Verbose)
+        {
+            if (Level < level)
+                return;
+
+            Console.ForegroundColor = GetConsoleColor(level);
+            if (newLine)
+                Console.WriteLine(message);
+            else
+                Console.Write(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static ConsoleColor GetConsoleColor(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Verbose: return ConsoleColor.DarkGray;
+                case LogLevel.Info: return ConsoleColor.White;
+                case LogLevel.Error: return ConsoleColor.Red;
+            }
+            return ConsoleColor.Gray;
+        }
+
+    }
+}
---
+++ Automatr/AutomatrOptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandLine;
+
+namespace Automatr
+{
+    public class AutomatrOptions
+    {
+
+        [Option('v', "version", Required = false, HelpText = "Print version number")]
+        public bool Version { get; set; }
+
+        [Option("verbose", Required = false, HelpText = "Enable verbose output")]
+        public bool Verbose { get; set; }
+
+        [Option('c', "config", Required = false, HelpText = "Config Path")]
+        public string ConfigPath { get; set; }
+
+        [Option('f', "force", Required = false, HelpText = "Force running of tasks")]
+        public bool Force { get; set; }
+
+        public AutomatrOptions()
+        {
+            ConfigPath = "AutomatrConfig.xml";
+        }
+
+    }
+}
