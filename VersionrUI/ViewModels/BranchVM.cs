f6219e39fb5611898fb334e44104e6e24a0acfd5
acrawford <acrawford@versionr>
2016-03-16 10:47:59 +1100

    Force refresh after changing history limit

--- VersionrUI/ViewModels/BranchVM.cs
+++ VersionrUI/ViewModels/BranchVM.cs
@@ -18,6 +18,7 @@ namespace VersionrUI.ViewModels
         private string _searchText;
         private VersionVM _selectedVersion;
         private int _revisionLimit = 50;
+        private bool _forceRefresh = false;
         private static Dictionary<int, string> _revisionLimitOptions = new Dictionary<int, string>()
         {
             { 50, "50" },
@@ -71,7 +72,7 @@ namespace VersionrUI.ViewModels
         {
             get
             {
-                if (_history == null)
+                if (_history == null || _forceRefresh)
                     Load(() => Refresh());
                 if (!string.IsNullOrEmpty(SearchText))
                     return FilterHistory(_history, SearchText);
@@ -97,6 +98,7 @@ namespace VersionrUI.ViewModels
                 if (_revisionLimit != value)
                 {
                     _revisionLimit = value;
+                    _forceRefresh = true;
                     NotifyPropertyChanged("RevisionLimit");
                     NotifyPropertyChanged("History");
                 }
@@ -130,6 +132,8 @@ namespace VersionrUI.ViewModels
         {
             lock (refreshLock)
             {
+                _forceRefresh = false;
+
                 var headVersion = _areaVM.Area.GetBranchHeadVersion(_branch);
                 int? limit = (RevisionLimit != -1) ? RevisionLimit : (int?)null;
                 List<Version> versions = _areaVM.Area.GetHistory(headVersion, limit);
