55df31093dc034d90df2a5e33f94496a8fd09290
Alex <Alex@versionr>
2016-07-06 11:51:58 +1000

    Vsr2Git is now a remote provider

--- Vsr2Git/Vsr2GitOptions.cs
+++ Vsr2Git/Vsr2GitOptions.cs
@@ -9,8 +9,6 @@ namespace Vsr2Git
 {
 	public class Vsr2GitOptions
 	{
-		[VerbOption("replicate-to-git", HelpText = "Replicate Versionr changes to a git repository")]
-		public Commands.ReplicateToGitOptions ReplicateToGit { get; set; }
         [HelpOption]
         public string GetUsage()
         {
