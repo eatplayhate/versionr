f2a1e548dea28910bc18201654002c9f20991ae9
Alex <Alex@versionr>
2016-07-06 14:41:11 +1000

    VersionrWeb initial commit

---
+++ VersionrWeb/Program.cs
@@ -0,0 +1,22 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionrWeb
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var host = new NancyHost(new Uri("http://localhost:8086")))
+			{
+				host.Start();
+				Console.WriteLine("VersionrWeb is running...");
+				Console.ReadLine();
+			}
+		}
+	}
+}
