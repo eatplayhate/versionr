cb508b88d1997f916df2487af7ff2e465b078dd5
Alex <Alex@versionr>
2016-07-06 14:50:59 +1000

    Custom bootstrapper for hot reload templates

---
+++ VersionrWeb/Bootstrap/CustomBootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionrWeb.Bootstrap
+{
+	public class CustomBootstrapper : DefaultNancyBootstrapper
+	{
+		protected override IRootPathProvider RootPathProvider
+		{
+			get
+			{
+				return new HotReloadRootPathProvider();
+			}
+		}
+	}
+}
