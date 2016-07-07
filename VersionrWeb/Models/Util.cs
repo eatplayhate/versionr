dfafc0b8c7666f71d75f4f33ab4991f3022aad6b
Alex <Alex@versionr>
2016-07-07 22:50:25 +1000

    Version and DateTime formatting

---
+++ VersionrWeb/Models/Util.cs
@@ -0,0 +1,25 @@
+﻿using Nancy.ViewEngines.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionrWeb.Models
+{
+	public static class Util
+	{
+		public static IHtmlString ShortGuid(Guid guid)
+		{
+			var s = guid.ToString().Substring(0, 8);
+			return new NonEncodedHtmlString(s);
+		}
+
+		public static IHtmlString ApproxRelativeTime(DateTime dateTime)
+		{
+			var s = dateTime.ToRelativeDateString(true);
+			return new NonEncodedHtmlString(s);
+		}
+
+	}
+}
