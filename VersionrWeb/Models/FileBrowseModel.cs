4b65fa51b4df7bd004bbeaa3e2ad77f31bf099da
Alex <Alex@versionr>
2016-07-09 16:26:40 +1000

    Refactor directory browse; add file browse

---
+++ VersionrWeb/Models/FileBrowseModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Versionr;
+
+namespace VersionrWeb.Models
+{
+	public class FileBrowseModel
+	{
+		public string Name;
+		public long Size;
+
+		public bool IsContentText;
+		public string ContentText;
+
+		public string HighlightClass;
+
+		public FileBrowseModel(Area area, Versionr.Objects.Version version, Versionr.Objects.Record record)
+		{
+			Name = Path.GetFileName(record.CanonicalName);
+			Size = record.Size;
+
+			area.GetMissingRecords(new Versionr.Objects.Record[] { record });
+			using (var stream = area.ObjectStore.GetRecordStream(record))
+			{
+				stream.Position = 0;
+				var encoding = GuessEncoding(stream);
+				if (encoding == null)
+				{
+					IsContentText = false;
+				}
+				else
+				{
+					IsContentText = true;
+					byte[] bytes = new byte[stream.Length];
+					stream.Read(bytes, 0, bytes.Length);
+					ContentText = encoding.GetString(bytes);
+					HighlightClass = GuessHighlightClass(record.CanonicalName, ContentText);
+				}
+			}
+		}
+
+		private static Encoding GuessEncoding(Stream stream)
+		{
+			// Read Unicode byte order mark and reset stream
+			byte[] bom = new byte[4];
+			stream.Read(bom, 0, 4);
+			stream.Position = 0;
+
+			if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+			if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+			if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+			if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+			if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
+			
+			// No BOM, check if first four bytes are ASCII printable
+			foreach (byte b in bom)
+			{
+				if (b < 32 || b > 127)
+					return null;
+			}
+
+			// Assume UTF-8
+			return Encoding.UTF8;
+		} 
+
+		private static string GuessHighlightClass(string filename, string text)
+		{
+			switch (Path.GetExtension(filename).ToLower())
+			{
+				case ".cs":	return "cs";
+				default: return "";
+			}
+		}
+
+
+	}
+}
--- VersionrWeb/Modules/BrowseModule.cs
+++ VersionrWeb/Modules/BrowseModule.cs
@@ -1,9 +1,12 @@
 ﻿using Nancy;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Versionr;
 using VersionrWeb.Models;
 
 namespace VersionrWeb.Modules
@@ -12,18 +15,66 @@ namespace VersionrWeb.Modules
 	{
 		public BrowseModule()
 		{
-			Get["/src"] = _ => { return CreateView(null, null); };
-			Get["/src/{version}"] = ctx => { return CreateView(null, ctx.version); };
+			Get["/src"] = _ => { return CreateView("", null); };
+			Get["/src/{version}"] = ctx => { return CreateView("", ctx.version); };
 			Get["/src/{version}/{path*}"] = ctx => { return CreateView(ctx.path, ctx.version); };
 		}
 
-		private dynamic CreateView(string path, string version)
+		private dynamic CreateView(string path, string branchOrVersion)
 		{
-			var model = new BrowseModel(path, version);
-			if (model.IsDirectory)
-				return View["Browse/Directory", model];
-			else
-				return View["Browse/File", model];
+			// Set common view properties
+			ViewBag.RepositoryName = Path.GetFileName(Environment.CurrentDirectory);
+			ViewBag.ParentPath = path == "" ? null : string.Format("/src/{0}/{1}", branchOrVersion, Path.GetDirectoryName(path).Replace('\\', '/'));
+			ViewBag.Breadcrumbs = path.Split('/');
+			ViewBag.BreadcrumbBasePath = string.Format("/src/{0}", branchOrVersion);
+
+			// Load area
+			var area = Versionr.Area.Load(new DirectoryInfo(Environment.CurrentDirectory), true);
+
+			// Default branch
+			if (branchOrVersion == null)
+				branchOrVersion = "master";
+
+			// Decode version or lookup head of branch
+			Guid versionId;
+			if (!Guid.TryParse(branchOrVersion, out versionId))
+			{
+				var branch = area.GetBranchByName(branchOrVersion).FirstOrDefault();
+				versionId = area.GetBranchHead(branch).Version;
+			}
+
+			var version = area.GetVersion(versionId);
+			var records = area.GetRecords(version);
+
+			// Normalize path (we don't know if it's a file or directory from the URL)
+			path = path.TrimEnd('/');
+
+			// Root directory
+			if (path == "")
+			{
+				return CreateDirectoryView(area, version, branchOrVersion, records, "");
+			}
+
+			// Find record at path
+			var record = records.Where(x => x.CanonicalName == path).FirstOrDefault();
+			if (record != null)
+			{
+				return CreateFileView(area, version, record);
+			}
+
+			// Not found as file, must be a directory
+			return CreateDirectoryView(area, version, branchOrVersion, records, path);
+		}
+
+		private dynamic CreateDirectoryView(Area area, Versionr.Objects.Version version, string branchOrVersion, List<Versionr.Objects.Record> records, string path)
+		{
+			var directoryRecords = records.Where(x => Path.GetDirectoryName(x.CanonicalName.TrimEnd('/')).Replace('\\', '/') == path).ToList();
+			return View["Browse/Directory", new DirectoryBrowseModel(area, version, branchOrVersion, path, directoryRecords)];
+		}
+
+		private dynamic CreateFileView(Area area, Versionr.Objects.Version version, Versionr.Objects.Record record)
+		{
+			return View["Browse/File", new FileBrowseModel(area, version, record)];
 		}
 	}
 }
