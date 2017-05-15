828aa7239f06299c8c4cfd5c122c70578e95ba16
lstrudwick <lstrudwick@versionr>
2017-05-15 17:06:31 +1000

    Fixed issue where ignored files are extra exciting

--- VersionrCore/Area.cs
+++ VersionrCore/Area.cs
@@ -7239,7 +7239,7 @@ namespace Versionr
                                         }
                                         break;
                                     case StatusCode.Ignored:
-                                        if (x.VersionControlRecord != null || (stagedOps != null && stagedOps.Count > 0 && stagedOps.Any(so => so.Type == StageOperationType.Remove)))
+                                        if (stagedOps != null && stagedOps.Count > 0 && stagedOps.Any(so => so.Type == StageOperationType.Remove))
                                         {
                                             Printer.PrintMessage("Removed (ignored): #b#{0}##", x.VersionControlRecord.CanonicalName);
                                             Printer.PrintDiagnostics("Recorded removal: {0}, old record: {1}", x.VersionControlRecord.CanonicalName, x.VersionControlRecord.Id);
@@ -7250,15 +7250,27 @@ namespace Versionr
                                         }
                                         else if (stagedOps != null && stagedOps.Count > 0 && stagedOps.Any(so => so.Type == StageOperationType.MergeRecord))
                                         {
-                                            x.VersionControlRecord = GetRecord(stagedOps.First(so => so.Type == StageOperationType.MergeRecord).ReferenceObject);
                                             if (x.VersionControlRecord != null)
                                             {
-                                                Printer.PrintMessage("Discarded (ignored merge result): #b#{0}##", x.VersionControlRecord.CanonicalName);
-                                                Printer.PrintDiagnostics("Recorded discard: {0}, old record: {1}", x.VersionControlRecord.CanonicalName, x.VersionControlRecord.Id);
+                                                Printer.PrintMessage("Removed (ignored-merge): #b#{0}##", x.VersionControlRecord.CanonicalName);
+                                                Printer.PrintDiagnostics("Recorded removal: {0}, old record: {1}", x.VersionControlRecord.CanonicalName, x.VersionControlRecord.Id);
                                                 Objects.Alteration alteration = new Alteration();
                                                 alteration.PriorRecord = x.VersionControlRecord.Id;
-                                                alteration.Type = AlterationType.Discard;
+                                                alteration.Type = AlterationType.Delete;
                                                 alterations.Add(alteration);
+                                            }
+                                            else
+                                            {
+                                                x.VersionControlRecord = GetRecord(stagedOps.First(so => so.Type == StageOperationType.MergeRecord).ReferenceObject);
+                                                if (x.VersionControlRecord != null)
+                                                {
+                                                    Printer.PrintMessage("Discarded (ignored merge result): #b#{0}##", x.VersionControlRecord.CanonicalName);
+                                                    Printer.PrintDiagnostics("Recorded discard: {0}, old record: {1}", x.VersionControlRecord.CanonicalName, x.VersionControlRecord.Id);
+                                                    Objects.Alteration alteration = new Alteration();
+                                                    alteration.PriorRecord = x.VersionControlRecord.Id;
+                                                    alteration.Type = AlterationType.Discard;
+                                                    alterations.Add(alteration);
+                                                }
                                             }
                                         }
                                         break;
