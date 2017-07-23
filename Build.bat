aa0f7f0af2b918f99f6a32acb8b1d14f9ac1acf1
jrunting <jrunting@versionr>
2017-07-24 09:32:51 +1000

    different build location

--- Build.bat
+++ Build.bat
@@ -1,4 +1,8 @@
 cd /d %~dp0
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" Versionr.sln /Rebuild Release
-mkdir Z:\Temp\LStrudwick\Versionr-Release
-robocopy bin Z:\Temp\LStrudwick\Versionr-Release /MIR
+mkdir P:\Osiris\Versionr
+robocopy bin P:\Osiris\Versionr /MIR
+
+
+
+
